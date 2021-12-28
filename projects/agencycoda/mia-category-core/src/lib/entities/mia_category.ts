94c8417ff73c39898de085223720c285963ede63
Matias Camiletti <matias.camiletti@gmail.com>
2021-12-27 22:07:57 -0300

    Update entity.

--- projects/agencycoda/mia-category-core/src/lib/entities/mia_category.ts
+++ projects/agencycoda/mia-category-core/src/lib/entities/mia_category.ts
@@ -8,4 +8,7 @@ export class MiaCategory extends MiaModel {
     icon: string = '';
     type: number = 0;
     is_featured: number = 0;
+    caption?: string = '';
+    is_show?: number = 1;
+    parent_id?: number;
 }
